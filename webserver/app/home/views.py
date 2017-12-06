9b273412d3b7e3a0ea42a06a1d5bde022b5d8a2f
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-12-06 17:09:38 -0500

    add other rules for models

--- webserver/app/home/views.py
+++ webserver/app/home/views.py
@@ -20,3 +20,22 @@ def show_users():
 		res += "<p>" + str(user) + ", " + user.location + "</p>"
 
 	return res
+
+@home.route("/books")
+def show_books():
+	res = ""
+	for book in models.Book.query.limit(5).all():
+		res += "<p>" + str(book) + ", " + book.title + "</p>"
+
+	return res
+
+@home.route("/ratings")
+def show_ratings():
+	joined = models.Rating.query.join(models.Book, models.Rating.uid == models.Book.isbn)
+	print(joined)
+
+	res = ""
+	for rating in joined.limit(5).all():
+		res += "<p>" + rating.title + ", " + str(rating.rating) + "</p>"
+
+	return res
