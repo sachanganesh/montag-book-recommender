1c94d692767c477f1aaf6f55f9b8da7b1f806ad1
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-12-07 14:07:43 -0500

    add flask-login utils

--- webserver/app/__init__.py
+++ webserver/app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+
+login_manager = LoginManager()
 
 def create_app(config_name):
 	app = Flask(__name__, instance_relative_config=True)
@@ -20,7 +23,14 @@ def create_app(config_name):
 
 	migrate = Migrate(app, db)
 
+	login_manager.init_app(app)
+	login_manager.login_view = "home.showSignin"
+
 	from app import models
+
+	@login_manager.user_loader
+	def load_user(user_id):
+		return models.User.query.filter(models.User.id == int(user_id)).first()
 
 	from .home import home as home_blueprint
 	app.register_blueprint(home_blueprint)
