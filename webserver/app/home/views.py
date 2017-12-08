a3c4e6853d511473555b32b6b98e6955777dd6bf
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-12-07 22:17:36 -0500

    setup book lookup pages

--- webserver/app/home/views.py
+++ webserver/app/home/views.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_required
+from string import ascii_uppercase
+from app import models
 from . import home
 
 @home.route("/")
@@ -12,3 +14,10 @@ def index():
 @login_required
 def protect():
 	return "hey it's a secret!"
+
+@home.route("/book-lookup/<letter>")
+# @login_required
+def book_lookup(letter):
+	letter = letter.upper()[0]
+	books = models.Book.query.filter(models.Book.title.startswith(letter)).all()
+	return render_template("home/lookup.html", letter=letter, books=books, ascii_upper=list(ascii_uppercase))
