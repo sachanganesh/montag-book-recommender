91d1b336295971993688bf41abfc3da49ac16174
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-12-08 03:10:39 -0500

    complete like/dislike button

--- webserver/app/home/views.py
+++ webserver/app/home/views.py
@@ -10,24 +10,23 @@ from . import home
 def index():
 	return render_template("home/index.html")
 
-@home.route("/protected")
-@login_required
-def protect():
-	return "hey it's a secret!"
-
 @home.route("/book-lookup/<letter>")
-# @login_required
+@login_required
 def book_lookup(letter="a"):
 	letter = letter.upper()[0]
 	books = models.Book.query.filter(models.Book.title.startswith(letter)).all()
 	if books is not None:
-		return render_template("home/lookup.html", letter=letter, books=books, ascii_upper=list(ascii_uppercase))
+		liked_isbns = []
+		for rating in current_user.ratings:
+			liked_isbns.append(rating.isbn)
+
+		return render_template("home/lookup.html", letter=letter, books=books, liked_isbns=liked_isbns, ascii_upper=list(ascii_uppercase))
 	else:
 		flash("Please search for a valid book entry.")
 		return redirect(url_for("home.book_lookup"))
 
 @home.route("/book/<isbn>")
-# @login_required
+@login_required
 def render_book(isbn):
 	book = models.Book.query.filter_by(isbn=isbn).first()
 
